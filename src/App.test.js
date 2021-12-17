f5d68442dfb84e6014a4f91972339015ec7344bc
Евгений Нифантьев <nifantyev@gmail.com>
2021-12-17 23:05:22 +0300

    Обновил тест

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders add to cart button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/добавить в корзину/i);
+  expect(buttonElement).toBeInTheDocument();
 });
